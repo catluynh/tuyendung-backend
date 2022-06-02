909ea1dd1b3d4ea68e9d627eb9c309b6129a3afe
catluynh <catluynh99@gmail.com>
2022-06-02 18:41:38 +0700

    add

--- utils/email.js
+++ utils/email.js
@@ -2,14 +2,13 @@ const nodemailer = require('nodemailer');
 const guiEmail = async (options) => {
   // 1. Create a transporter
   let transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+    service: 'gmail.com',
+    secure: false,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
     },
-    secure: false
-    // Active in gmail "less secure app" option
+    // secure: false
   });
   // 2. Define the email option
   const mailOptions = {
