e7e38828a44dab1d05077e17d6f4b6659eeac229
catluynh <catluynh99@gmail.com>
2022-06-02 23:31:47 +0700

    a

--- utils/email.js
+++ utils/email.js
@@ -2,13 +2,13 @@ const nodemailer = require('nodemailer');
 const guiEmail = async (options) => {
   // 1. Create a transporter
   let transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: true,
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
-      user: process.env.EMAIL_USERNAME || 'catluynh1999@gmail.com',
-      pass: process.env.EMAIL_PASSWORD || '0903589164',
-    },
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    }
   });
   // 2. Define the email option
   const mailOptions = {
